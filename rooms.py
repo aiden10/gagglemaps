2b9ea8971c6d76166310cdb50b8f54e695eb9cc7
aiden10 <aiden.ten30@gmail.com>
2025-01-25 16:52:02 -0500

    remove parser, add some data

--- rooms.py
+++ rooms.py
@@ -1,46 +1,26 @@
-import pymupdf
-import os
-import platform
-from tempfile import TemporaryDirectory
-from pathlib import Path
-import pytesseract
-from pdf2image import convert_from_path
-from PIL import Image
-
-if platform.system() == "Windows":
-    pytesseract.pytesseract.tesseract_cmd = (
-        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    )
-    path_to_poppler_exe = Path(r"C:\.....")
-    out_directory = Path(r"~\Desktop").expanduser()
-else:
-    out_directory = Path("~").expanduser()  
-
-def get_text(PDF_file):
-    image_file_list = []
-    with TemporaryDirectory() as tempdir:
-        if platform.system() == "Windows":
-            pdf_pages = convert_from_path(PDF_file, 500, poppler_path=path_to_poppler_exe)
-        else:
-            pdf_pages = convert_from_path(PDF_file, 500)
-        
-        for page_enumeration, page in enumerate(pdf_pages, start=1):
-            filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
-            page.save(filename, "JPEG")
-            image_file_list.append(filename)
-        
-        for image_file in image_file_list:
-            text = str(((pytesseract.image_to_string(Image.open(image_file)))))
-            text = text.replace("-\n", "")
-            print(text)
-    
-    with pymupdf.open(PDF_file) as doc:
-        text = chr(12).join([page.get_text() for page in doc])
-        print(text)
-
-for file in os.listdir("floorPlans"):
-    filename = os.fsdecode(file)
-    if filename.endswith(".pdf"):
-        fn = os.path.join("floorPlans", filename)
-        print(fn)
-        get_text(fn)
+buildings_dict = {
+    'AL': 'Arts Lecture Hall',
+    'B1': 'Biology 1',
+    'B2': 'Biology 2',
+    'DC': 'William G. Davis Computer Research Centre',
+    'DWE': 'Douglas Wright Engineering Building',
+    'E2': 'Engineering 2',
+    'EIT': 'Centre for Environmental and Information Technology',
+    'EV1': 'Environment 1',
+    'EV2': 'Environment 2',
+    'EV3': 'Environment 3',
+    'LHI': 'Lyle S. Hallman Institute for Health Promotion ',
+    'HH': 'J.G. Hagey Hall of the Humanities',
+    'M3': 'Mathematics 3',
+    'MC': 'Mathematics and Computer Building',
+    'ML': 'Modern Languages',
+    'OPT': 'Optometry Building',
+    'PAS': 'Psychology, Anthropology, Sociology',
+    'PHY': 'Physics',
+    'QNC': 'Quantum Nano Centre',
+    'RCH': 'J.R. Coutts Engineering Lecture Hall',
+    'STC': 'Science Teaching Complex',
+    'UTD': 'United College'
+}
+RCH = ["RCH-101", "RCH-103", "RCH-105", "RCH-106", "RCH-109", "RCH-110", "RCH-112", "RCH-204", "RCH-205", "RCH-206", "RCH-207", "RCH-208", "RCH-209", "RCH-211", "RCH-212", "RCH-301", "RCH-302", "RCH-305", "RCH-306", "RCH-307", "RCH-308", "RCH-309"]
+STC = ["STC-0010", "STC-0020", "STC-0040", "STC-0050", "STC-0060", "STC-1012"]
