9a10fe54ac8b890438aef185201d9afd5aeb9998
Hy Lac <121268118+kingMonkeh@users.noreply.github.com>
2025-01-25 17:09:51 -0500

    Basic room dataset added

--- rooms.py
+++ rooms.py
@@ -14,7 +14,6 @@ buildings_dict = {
     'M3': 'Mathematics 3',
     'MC': 'Mathematics and Computer Building',
     'ML': 'Modern Languages',
-    'OPT': 'Optometry Building',
     'PAS': 'Psychology, Anthropology, Sociology',
     'PHY': 'Physics',
     'QNC': 'Quantum Nano Centre',
@@ -22,5 +21,25 @@ buildings_dict = {
     'STC': 'Science Teaching Complex',
     'UTD': 'United College'
 }
+
+AL = ["AL-105", "AL-113", "AL-116", "AL-124", "AL-208", "AL-209", "AL-210", "AL-211"]
+B1 = ["B1-271"]
+B2 = ["B2-350"]
+DC = ["DC-1350", "DC-1351"]
+DWE = ["DWE-1501", "DWE-1502", "DWE-1515", "DWE-2527", "DWE-3516", "DWE-3517", "DWE-3518", "DWE-3519", "DWE-3522", "DWE-3522A"]
+E2 = ["E2-1732", "E2-1736"]
+EIT = ["EIT-1015"]
+EV1 = ["EV1-132", "EV1-225", "EV1-350"]
+EV2 = ["EV2-2002"]
+EV3 = []
+LHI = ["LHI-1689"]
+HH = ["HH-1101", "HH-1102", "HH-1104", "HH-1106", "HH-1108", "HH-119", "HH-123", "HH-124", "HH-138", "HH-139", "HH-150", "HH-2107", "HH-227", "HH-259", "HH-334", "HH-336", "HH-344", "HH-345"]
+M3 = ["M3-1006"]
+MC = ["MC-1056", "MC-1085", "MC-2017", "MC-2034", "MC-2035", "MC-2038", "MC-2054", "MC-2065", "MC-2066", "MC-4020", "MC-4021", "MC-4040", "MC-4041", "MC-4042", "MC-4045", "MC-4058", "MC-4059", "MC-4060", "MC-4061", "MC-4063", "MC-4064"]
+ML = ["ML-242", "ML-246", "ML-311", "ML-315", "ML-349", "ML-354"]
+PAS = ["PAS-1229", "PAS-1241", "PAS-2083", "PAS-2084", "PAS-2085", "PAS-2086"]
+PHY = ["PHY-145", "PHY-150", "PHY-235", "PHY-313"]
+QNC = ["QNC-1502", "QNC-1506", "QNC-1507", "QNC-2501", "QNC-2502"]
 RCH = ["RCH-101", "RCH-103", "RCH-105", "RCH-106", "RCH-109", "RCH-110", "RCH-112", "RCH-204", "RCH-205", "RCH-206", "RCH-207", "RCH-208", "RCH-209", "RCH-211", "RCH-212", "RCH-301", "RCH-302", "RCH-305", "RCH-306", "RCH-307", "RCH-308", "RCH-309"]
 STC = ["STC-0010", "STC-0020", "STC-0040", "STC-0050", "STC-0060", "STC-1012"]
+UTD = ["UTD-105"]
